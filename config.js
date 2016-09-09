d737ac1142461b0c9d2df0c01907a4591b8f4959
Erik Lieben <erik.lieben@effectory.com>
2016-09-09 22:28:19 +0200

    fix: usage with jspm

--- config.js
+++ config.js
@@ -8,6 +8,7 @@ System.config({
 
   map: {
     "aurelia-polyfills": "npm:aurelia-polyfills@1.0.0",
+    "interact.js": "npm:interact.js@1.2.6",
     "npm:aurelia-polyfills@1.0.0": {
       "aurelia-pal": "npm:aurelia-pal@1.0.0"
     }
