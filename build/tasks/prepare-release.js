841c54833d73a7ceb9036774589fa13271e2122f
Erik Lieben <erik.lieben@effectory.com>
2016-09-09 21:59:35 +0200

    chore(build) add conventional-changelog gulp task

--- build/tasks/prepare-release.js
+++ build/tasks/prepare-release.js
@@ -1,10 +1,10 @@
 var gulp = require('gulp');
 var runSequence = require('run-sequence');
 var paths = require('../paths');
-var changelog = require('conventional-changelog');
 var fs = require('fs');
 var bump = require('gulp-bump');
 var args = require('../args');
+var conventionalChangelog = require('gulp-conventional-changelog');
 
 // utilizes the bump plugin to bump the
 // semver for the repo
@@ -16,16 +16,13 @@ gulp.task('bump-version', function() {
 
 // generates the CHANGELOG.md file based on commit
 // from git commit messages
-gulp.task('changelog', function(callback) {
-  var pkg = JSON.parse(fs.readFileSync('./package.json', 'utf-8'));
-
-  return changelog({
-    repository: pkg.repository.url,
-    version: pkg.version,
-    file: paths.doc + '/CHANGELOG.md'
-  }, function(err, log) {
-    fs.writeFileSync(paths.doc + '/CHANGELOG.md', log);
-  });
+gulp.task('changelog', function () {
+  return gulp.src(paths.doc + '/CHANGELOG.md', {
+    buffer: false
+  }).pipe(conventionalChangelog({
+    preset: 'angular'
+  }))
+  .pipe(gulp.dest(paths.doc));
 });
 
 // calls the listed sequence of tasks in order
