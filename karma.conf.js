9851553968944bea29cde1fcb31f845c3a89852c
Erik Lieben <erik.lieben@effectory.com>
2016-09-10 08:25:37 +0200

    chore: update npm packages

--- karma.conf.js
+++ karma.conf.js
@@ -9,7 +9,7 @@ module.exports = function(config) {
           "typescript": "node_modules/typescript/lib/typescript.js",
           "systemjs": "node_modules/systemjs/dist/system.js",
           'system-polyfills': 'node_modules/systemjs/dist/system-polyfills.js',
-          'es6-module-loader': 'node_modules/es6-module-loader/dist/es6-module-loader.js'
+          'es-module-loader': 'node_modules/es6-module-loader/dist/es6-module-loader.js'
         },
         packages: {
           'test': {
