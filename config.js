41c9cdd96fb69bcbaf55522896bcca6383de609f
John <john.stephenson@cedus.co.uk>
2016-12-08 07:36:36 +0000

    Corrected erronous mapping sin config meaning that the package couldn't be installed

--- config.js
+++ config.js
@@ -7,11 +7,10 @@ System.config({
   },
 
   map: {
-    "aurelia-polyfills": "npm:aurelia-polyfills@1.1.0",
+    "aurelia-polyfills": "npm:aurelia-polyfills@1.1.1",
     "interact": "npm:interact.js@1.2.6",
-    "interact.js": "npm:interact.js@1.2.6",
-    "npm:aurelia-polyfills@1.1.0": {
-      "aurelia-pal": "npm:aurelia-pal@1.0.0"
+    "npm:aurelia-polyfills@1.1.1": {
+      "aurelia-pal": "npm:aurelia-pal@1.1.1"
     }
   }
 });
